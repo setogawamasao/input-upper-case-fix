25a42c5bd652d00d2b662a406ec5b3cdfe014a62
masaosetogawa <ms.job.hunting@gmail.com>
2020-10-30 16:51:37 +0900

    build

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <h1>Hello Ime Control</h1>
+      <h1>Hello Input Upper Case Fix </h1>
       <div>{"textTransform : uppercase"}</div>
       <input type="text" style={{ textTransform: "uppercase" }} />
     </>
